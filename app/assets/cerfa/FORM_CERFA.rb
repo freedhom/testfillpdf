18ebaa261d4a8f16c4449f8476c316e475857ea1
olivier Tengherian <oliver.hyperion@gmx.fr>
2020-06-16 03:16:11 +0200

    Watermark ok

---
+++ app/assets/cerfa/FORM_CERFA.rb
@@ -0,0 +1,52 @@
+{
+ -- OBJET de la DEMANDE --
+ :"topmostSubform[0].Page1[0].D6A_CUA[0]"=>"",
+ :"topmostSubform[0].Page1[0].D6B_CUB[0]"=>"",
+
+ -- PARTICULIER --
+ :"topmostSubform[0].Page1[0].D1F_femme[0]"=>"",
+ :"topmostSubform[0].Page1[0].D1H_homme[0]"=>"",
+ :"topmostSubform[0].Page1[0].D1N_nom[0]"=>"",
+ :"topmostSubform[0].Page1[0].D1P_prenom[0]"=>"",
+
+ -- PERSONNE MORALE --
+ :"topmostSubform[0].Page1[0].D2D_denomination[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2R_raison[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2S_SIRET[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2J_type[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2F_madame[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2H_monsieur[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2N_nom[0]"=>"",
+ :"topmostSubform[0].Page1[0].D2P_prenom[0]"=>"",
+
+ -- COORDONNEES --
+ :"topmostSubform[0].Page1[0].D3N_numero[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3V_voie[0]"=>"",
+ :exit,
+ :"topmostSubform[0].Page1[0].D3L_localite[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3C_code[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3B_boite[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3X_cedex[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3T_telephone[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3K_indicatif[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3P_pays[0]"=>"",
+ :"topmostSubform[0].Page1[0].D3D_division[0]"=>"",
+
+ -- EMAIL --
+ :"topmostSubform[0].Page1[0].D5A_acceptation[0]"=>"",
+ :"topmostSubform[0].Page1[0].D5GE1_email[0]"=>"",
+ :"topmostSubform[0].Page1[0].D5GE2_email[0]"=>"",
+
+ -- TERRAIN --
+ :"topmostSubform[0].Page1[0].T2Q_numero[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2V_voie[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2W_lieudit[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2L_localite[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2C_code[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2B_boite[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2X_cedex[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2F_prefixe[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2S_section[0]"=>"",
+ :"topmostSubform[0].Page1[0].T2N_numero[0]"=>"",
+ :"topmostSubform[0].Page1[0].D5T_total[0]"=>"",
+ }
--- app/controllers/fillforms_controller.rb
+++ app/controllers/fillforms_controller.rb
@@ -1,33 +1,34 @@
+require 'combine_pdf'
 require 'fillable-pdf'
 # install gem poppler
 # require 'pdftoimage'
-# require 'combine_pdf'
 
 class FillformsController < ApplicationController
-  PATH_CERFA << '/app/assets/cerfa/'
-  CERFA << 'cerfa.pdf'
-  RESULTPDF << 'resultpdf'
-  # PATH_CONTENTPDF = './app/assets/cerfa/contentpdf.pdf'
-  # PATH_WPARCELR = './app/assets/cerfa/WPARCLR.pdf'
+  PATH_CERFA = './app/assets/cerfa/'
+  CERFA = PATH_CERFA + 'cerfa.pdf'
+  RESULTPDF = PATH_CERFA + 'resultpdf'
+  CONTENTPDF = PATH_CERFA + 'content.pdf'
+  WPARCLR = PATH_CERFA + 'WPARCLR.pdf'
 
   def test
     setfields
-    showpdf
+    watermatkpdf(RESULTPDF, WPARCLR)
+    showpdf(CONTENTPDF)
   end
 
   private
 
-  def showpdf
-    pdf_filename = File.join(Rails.root, PATH_CERFA + RESULTPDF)
-    send_file(pdf_filename, filename: RESULTPDF, disposition: 'inline', type: 'application/pdf')
+  def showpdf(file)
+    pdf_filename = File.join(Rails.root, file)
+    send_file(pdf_filename, filename: 'resultpdf.pdf', disposition: 'inline', type: 'application/pdf')
   end
 
   def opencerfa
-    FillablePDF.new PATH_CERFA + CERFA
+    FillablePDF.new CERFA
   end
 
   def save(file)
-    file.save_as(PATH_CERFA + RESULTPDF, flatten: true)
+    file.save_as(RESULTPDF, flatten: true)
   end
 
   def setobject(file)
@@ -51,13 +52,13 @@ class FillformsController < ApplicationController
     save(cerfa)
   end
 
-  # # --- Watermark on PDF with : 'gem install combine_pdf'
-  # def watermatkpdf(file)
-  #   company_logo = CombinePDF.load(PATH_WPARCELR).pages[0]
-  #   pdf = CombinePDF.load PATH_RESULTPDF
-  #   pdf.pages.each { |page| page << company_logo }
-  #   pdf.save PATH_CONTENTPDF
-  # end
+  # --- Watermark on PDF with : 'gem install combine_pdf'
+  def watermatkpdf(source, logo)
+    company_logo = CombinePDF.load(logo).pages[0]
+    pdf = CombinePDF.load source
+    pdf.pages.each { |page| page << company_logo }
+    pdf.save CONTENTPDF
+  end
 
   # # --------- PDF to JPG
   # def pdftoimage(file)
